6bbe6940e5c82a5e3920347c1d277dcfa65b4713
llunareine <llunareine@gmail.com>
2022-04-08 01:36:03 +0600

    added components and added imports in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
-
-import './App.css';
+import React, { useState, useEffect} from 'react'
+import YouTube from '../api/YouTube';
+import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 function App() {
   return (
